97fcd2ce7cda60161e12213722a72b1440db170a
Gabriel <gabriel10919@outlook.com>
2024-08-14 15:17:25 -0300

    Instanciando a clareza.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from FactoryClass import FactoryConexao
 from InterfaceCLI import InterfaceCLI
+from StrateyClass import AvaliarClareza, ProcessadorRespostas
 
 if __name__ == "__main__":
     factory = FactoryConexao()
     cli = InterfaceCLI(factory)
     
-    cli.criar_interface()
+    respostas = cli.criar_interface()
     
+    # Avaliar clareza
+    avaliador_clareza = AvaliarClareza()
+    processador_clareza = ProcessadorRespostas(avaliador_clareza)
+    melhor_resposta_clareza = processador_clareza.processar(respostas)
+    print(f"Melhor resposta com base na clareza: {melhor_resposta_clareza}")
