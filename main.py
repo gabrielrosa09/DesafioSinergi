75ef3016119af850c654de67c7313d0d7d9f10cd
Gabriel Rosa <gabriel10919@outlook.com>
2024-08-14 16:11:51 -0300

    avaliador de comprimento das respostas.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from FactoryClass import FactoryConexao
 from InterfaceCLI import InterfaceCLI
-from StrateyClass import AvaliarClareza, ProcessadorRespostas
+from StrateyClass import AvaliarClareza, AvaliadorComprimentoResposta, ProcessadorRespostas
 
 if __name__ == "__main__":
     factory = FactoryConexao()
@@ -8,8 +8,12 @@ if __name__ == "__main__":
     
     respostas = cli.criar_interface()
     
-    # Avaliar clareza
     avaliador_clareza = AvaliarClareza()
     processador_clareza = ProcessadorRespostas(avaliador_clareza)
     melhor_resposta_clareza = processador_clareza.processar(respostas)
     print(f"Melhor resposta com base na clareza: {melhor_resposta_clareza}")
+    
+    avaliador_comprimento = AvaliadorComprimentoResposta()
+    processador_comprimento = ProcessadorRespostas(avaliador_comprimento)
+    resposta_maior_comprimento = processador_comprimento.processar(respostas)
+    print(f'Melhor resposta com base no comprimento: {resposta_maior_comprimento}')
