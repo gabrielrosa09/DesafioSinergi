521f8df2d60aa58d37e60c32166ae33b0add4f1f
Gabriel <gabriel10919@outlook.com>
2024-08-17 21:45:43 -0300

    Documentando o CommandClass

--- patterns/CommandClass.py
+++ patterns/CommandClass.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 from patterns.FactoryClass import ConexaoLLM
 
+# Classe abstrata CommandClass
 class CommandClass(ABC):
     
     @abstractmethod
     def enviar_prompt(self) -> str:
         pass
     
+# Classe EncapsularPrompt que herda de CommandClass
 class EncapsularPrompt(CommandClass):
     def __init__(self, conexao: ConexaoLLM, prompt: str):
         self.conexao = conexao
         self.prompt = prompt
     
     def enviar_prompt(self) -> str:
+        # Chamando o método conectar da classe ConexaoLLM
         return self.conexao.conectar(self.prompt)
