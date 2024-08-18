0573f841ec07813cc08b197ac180b976553cf6f2
Gabriel <gabriel10919@outlook.com>
2024-08-17 21:51:52 -0300

    Documentando a Interface CLI

--- interface_cli/InterfaceCLI.py
+++ interface_cli/InterfaceCLI.py
@@ -5,12 +5,13 @@ from rich.console import Console
 
 console = Console()
 
-
+# Classe InterfaceCLI que cria a interface de linha de comando
 class InterfaceCLI:
     def __init__(self, factory: FactoryConexao):
         self.factory = factory
         
     def criar_interface(self):
+        # Imprimindo a mensagem para o terminal
         console.print("# ------------------- #", style="bold blue")
         console.print(" Bem-vindo ao ChatBot! ", style="bold blue")
         console.print("# ------------------- #", style="bold blue")
@@ -19,9 +20,13 @@ class InterfaceCLI:
         respostas = []
         
         for modelo in modelos:
+            # Criando a conexão
             conexao = self.factory.criar_conexao(modelo)
+            # Encapsulando o prompt
             comando = EncapsularPrompt(conexao, prompt)
+            # Enviando o prompt
             resposta = comando.enviar_prompt()
+            # Adicionando a resposta à lista de respostas
             respostas.append((modelo, resposta))
         
         return respostas
