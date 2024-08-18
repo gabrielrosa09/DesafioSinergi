e6e184d9243f1e1aee257e9e4fc17c0e210caba5
Gabriel <gabriel10919@outlook.com>
2024-08-17 21:54:39 -0300

    Documentando main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from patterns.ObserverClass import ClienteObserver, ProcessadorRespostasObserver
 from rich.console import Console
 
 if __name__ == "__main__":
+    # Inicialização dos objetos
     console = Console()
     cliente_observer = ClienteObserver()
     factory = FactoryConexao()
@@ -15,22 +16,30 @@ if __name__ == "__main__":
     
     console.print("-- Estratégia de avaliação de comprimento de resposta --", style="bold red")
     
+    # Inicializando a estratégia de avaliação de comprimento de resposta
     strategyAvaliarComprimento = AvaliadorComprimentoResposta()
 
+    # Inicializando o processador de respostas
     processador = ProcessadorRespostasObserver(strategyAvaliarComprimento)
     
+    # Adicionando o observer
     processador.adicionar_observer(cliente_observer)
 
+    # Processando as respostas
     processador.processar(respostas)
     
     # Estratégia de avaliação de clareza de resposta
     
     console.print("-- Estratégia de avaliação de clareza de resposta --", style="bold red")
     
+    # Inicializando a estratégia de avaliação de clareza de resposta
     strategyAvaliarClareza = AvaliarClareza()
     
+    # Inicializando o processador de respostas
     processador = ProcessadorRespostasObserver(strategyAvaliarClareza)
 
+    # Adicionando o observer
     processador.adicionar_observer(cliente_observer)
 
+    # Processando as respostas
     processador.processar(respostas)
